3f40350d256ffbdb2cf2dc3028154528ec5c280a
sarancoder0499 <sarancoder0499@gmail.com>
2024-08-02 18:46:05 +0530

    Tested Case 2

--- src/Add.test.tsx
+++ src/Add.test.tsx
@@ -1,10 +1,24 @@
 import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { cleanup, render, screen } from "@testing-library/react";
 import Add from "./Add";
 
 describe("Add", () => {
-  it("it should return 0 if input value is empty", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("should return 0 if input value is empty", () => {
     render(<Add numbers="" />);
+    expect(screen.getByText("0")).toBeDefined();
+  });
+
+  it("should return 1 if input string length has 1 and it is a number", () => {
+    render(<Add numbers="1" />);
+    expect(screen.getByText("1")).toBeDefined();
+  });
+
+  it("should return 0 if input string length has 1 and it is not a number", () => {
+    render(<Add numbers="a" />);
     expect(screen.getByText("0")).toBeDefined();
   });
 });
--- src/Add.tsx
+++ src/Add.tsx
@@ -1,5 +1,6 @@
 export default function Add({ numbers }: { numbers: string }): number {
-  if (numbers != "") {
+  // check numbers has length 1 and it is number
+  if (numbers.length == 1 && /^\d$/.test(numbers[0])) {
     return parseInt(numbers);
   }
   return 0;
